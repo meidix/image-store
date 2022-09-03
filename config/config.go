eb613ebc8af5decb5b5b4871b644060eb28b5c66
meidix <mahdihossieni@gmail.com>
2022-09-03 23:17:42 +0300

    added config package and updated dependencies

--- config/config.go
+++ config/config.go
@@ -1,3 +1,7 @@
 package config
 
-const ServerAddress = "localhost:5000"
+
+const SERVER_ADDRESS = "localhost:5000"
+const USE_DB = true
+const MONGO_URI_NAME_ENV = "MONGO_URI"
+const MONGO_DB_NAME = "mediastorage"
